3f5b9f4cfb063266ad438cf31f062dbd8cc24d08
Alex Close <ajclose@gmail.com>
2017-07-09 16:27:19 -0500

    Connect signup page to database

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 mongoose.Promise = require('bluebird')
 
 const userSchema = new mongoose.Schema({
+  email: {type: String, required: true},
   username: {type: String, required: true},
   password: {type: String, required: true},
   activities: {
@@ -13,3 +14,7 @@ const userSchema = new mongoose.Schema({
     ]
   }
 })
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
 
 router.get('/', function(req, res) {
-  res.render('index')
+  res.render('login')
 })
 
 router.get('/signup', function(req, res) {
   res.render('signup')
+})
+
+router.post('/signup', function(req, res) {
+  const user = new User()
+  user.email = req.body.email
+  user.username = req.body.username
+  user.password = req.body.password
+  user.save()
+  .then(function(user) {
+    res.redirect('/')
+  })
+  .catch(function(error) {
+    res.render('signup')
+  })
 })
 
 module.exports = router;
