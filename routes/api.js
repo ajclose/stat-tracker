39fb0b359174053aa1db7d81f433756f169ac5d5
Alex Close <ajclose@gmail.com>
2017-07-12 16:40:28 -0500

    Add delete route

--- routes/api.js
+++ routes/api.js
@@ -38,5 +38,47 @@ router.get('/api/activities/:id', function(req, res) {
   })
 })
 
+router.post('/api/activities/:id/stats', function(req, res) {
+  const date = new Date()
+  Activity.findOne({
+    _id: req.params.id
+  })
+  .then(function(activity) {
+    activity.stats.push({
+      date: date,
+      number: req.body.number
+    })
+    activity.save()
+    .then(function(activity) {
+      res.json(activity)
+    })
+  })
+})
+
+router.put('/api/activities/:id', function(req, res) {
+  Activity.findOne({
+    _id: req.params.id
+  })
+  .then(function(activity) {
+    activity.title = req.body.title
+    activity.save()
+    .then(function(activity) {
+      res.json(activity)
+    })
+  })
+})
+
+router.delete('/api/activities/:id', function(req, res) {
+  Activity.deleteOne({
+    _id: req.params.id
+  })
+  .then(function(activity) {
+    res.json(activity)
+  })
+  .catch(function(error) {
+    res.json(error)
+  })
+})
+
 
 module.exports = router
