14d499e87fc48cbc75ea4b250f2ebae8e3371310
Alex Close <ajclose@gmail.com>
2017-07-12 19:40:58 -0500

    Display single activity with stats

--- public/js/client.js
+++ public/js/client.js
@@ -1,4 +1,5 @@
 function fetchActivities() {
+  document.querySelector(".activities").textContent = ""
   fetch("api/activities", {
   credentials: 'include'
 })
@@ -6,22 +7,58 @@ function fetchActivities() {
   return res.json()
 })
 .then(function(json) {
-  console.log(json);
 
   for (var i = 0; i < json.length; i++) {
     const activity = json[i]
 
     const html = `
-    <div class="game">
-    <a href="/api/activities/${activity._id}">
+    <div class="activity" id=${activity._id}>
     <h3>${activity.title}</h3>
     </a>
+    <p>Records: ${activity.stats.length}
     </div>
     `
 
     document.querySelector(".activities").insertAdjacentHTML("beforeend", html)
   }
 })
+.then(function() {
+  const activities = document.querySelectorAll(".activity")
+  for (var j = 0; j < activities.length; j++) {
+    const activity = activities[j]
+    activity.addEventListener("click", function(event) {
+      const id = activity.id
+      fetchActivity(id)
+    })
+  }
+})
+}
+
+function fetchActivity(id) {
+  document.querySelector(".activities").textContent = ""
+  fetch(`api/activities/${id}`, {
+    credentials: 'include'
+  })
+  .then(function(res) {
+    return res.json()
+  })
+  .then(function(json) {
+    const title = `<h2>${json.title}</h2>`
+    document.querySelector(".activities").insertAdjacentHTML("afterbegin", title)
+    const stats = json.stats
+    for (var i = 0; i < stats.length; i++) {
+      const stat = stats[i]
+      const html = `
+      <div class="stats">
+      <p class="date">${stat.date}</p>
+      <p class="number">${stat.number}</p>
+      </div>
+      `
+
+      document.querySelector(".activities").insertAdjacentHTML("beforeend", html)
+    }
+
+  })
 }
 
 const activityContainer = document.querySelector(".activities")
